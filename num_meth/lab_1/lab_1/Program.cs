4bdaec32e2af594bdc0ca128b8d63d133cb7b382
Miri <113782029+Miri-Solari@users.noreply.github.com>
2023-02-08 19:25:01 +0300

    глшл

---
+++ num_meth/lab_1/lab_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab_1
+{
+    class Program
+    {
+        static double F(double x)
+        {
+            return x * x * x - 2 - Math.Log(x + 2);
+        }
+        static void Main(string[] args)
+        {
+            double a, b, x0, x1, E;
+            Console.WriteLine("Input a ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input b ");
+            b = Convert.ToDouble(Console.ReadLine());
+            x0 = (a + b) / 2;
+            Console.WriteLine("Input E");
+            E = Convert.ToDouble(Console.ReadLine());
+            while (a - b > E)
+            {
+
+            }
+
+        }
+    }
+}
