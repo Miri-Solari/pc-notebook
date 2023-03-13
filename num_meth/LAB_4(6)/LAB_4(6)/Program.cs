556a8e6632999921ea45b4ce63f1c4cf008d9923
Miri-Solari <horniret@gmail.com>
2023-03-13 23:22:18 +0300

    Num_meth_4_end

---
+++ num_meth/LAB_4(6)/LAB_4(6)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LAB_4_6_
+{
+    class Program
+    {
+        static double F(double x)
+        {
+            return Math.Log(x * x + 2);
+        }
+        static double SRect(double h, double y)
+        {
+            return y*h;
+        }
+
+
+        static void Main(string[] args)
+        {
+            double a = 50;
+            double b = 100;
+            double n;
+            double h, answ = 0;
+
+            Console.WriteLine("Введите количество дроблений: ");
+            n = Convert.ToDouble(Console.ReadLine());
+            h = (a + b) / n;
+            for (double x = a; x <= b; x += h)
+            {
+                answ += SRect(h, F(x));
+            }
+            Console.WriteLine("Результат, полученный методом прямоугольников: " + Convert.ToString(answ));
+        }
+    }
+}
