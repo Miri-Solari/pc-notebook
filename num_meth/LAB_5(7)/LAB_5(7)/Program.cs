8e097ca0ed485b6595d70074daa229d9ae104dde
Miri-Solari <horniret@gmail.com>
2023-03-22 01:28:22 +0300

    0

---
+++ num_meth/LAB_5(7)/LAB_5(7)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LAB_5_7_
+{
+    class Program
+    {
+        static double F(double x)
+        {
+            return Math.Log(x * x + 2);
+        }
+        static double STrap(double h, double y0, double y1)
+        {
+            return (y0 + y1) / 2 * h;
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 100;
+            double n;
+            double h, answ = 0;
+            Console.WriteLine("Введите количество дроблений: ");
+            n = Convert.ToDouble(Console.ReadLine());
+            h = Math.Abs(a - b) / n;
+            for (int x = 0; x < n; x++)
+            {
+                answ += STrap(h, F(x * h + a), F(x * h + a + h));
+            }
+            Console.WriteLine("Результат, полученный методом Трапеций: " + Convert.ToString(answ));
+        }
+    }
+}
---
+++ num_meth/Lab_6(8)/Lab_6(8)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LAB_6_8_
+{
+    class Program
+    {
+        static double F(double x)
+        {
+            return Math.Log(x * x + 2);
+        }
+
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 100;
+            double n;
+            double h, answ = 0;
+            Console.WriteLine("Введите количество дроблений: ");
+            n = Convert.ToDouble(Console.ReadLine());
+            h = Math.Abs(a - b) / n;
+            answ += F(a) + F(b);
+            for (int x = 1; x < 2*n; x ++)
+            {
+                Console.WriteLine(F(x * h + a) * Math.Pow(2, 1 + x % 2));
+                answ += F(x*h + a)* Math.Pow(2, 1 + x % 2);
+            }
+            Console.WriteLine("Результат, полученный методом Симпсона: " + Convert.ToString(h/3*answ));
+        }
+    }
+}
