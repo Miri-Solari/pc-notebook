ca7d2e765cf9f2ce364c66b7a82ee8945335f0c0
Miri-Solari <horniret@gmail.com>
2023-11-03 18:05:41 +0300

    4LAB_OOP

---
+++ ООП/LAB_4/LAB_4/Stud.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_4
+{
+    internal class Stud
+    {
+        internal string Name { get; set; }
+        internal string SecondName
+        {
+            get { return SecondName; }
+            set { SecondName = value; }
+        }
+        internal int Age
+        {
+            get { return Age; }
+            set
+            {
+                if (value < 0) { value = 0; }
+                Age = value;
+            }
+        }
+
+        internal bool IsStudentDependOnAge
+        {
+            get { return Age < 25; }
+        }
+
+        internal string GetInfo()
+        {
+            return $"{Name} {SecondName} {Age} {IsStudentDependOnAge}";
+        }
+
+        internal string GetInfo(bool includeIntProperty)
+        {
+            if (includeIntProperty)
+            {
+                return $"{Name} {SecondName} {Age} {IsStudentDependOnAge}";
+            }
+            return $"{Name} {SecondName} {IsStudentDependOnAge}";
+        }
+
+    }
+}
